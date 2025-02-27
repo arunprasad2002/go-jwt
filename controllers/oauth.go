bdffc1b40f2678a043f7966f54c100036f3c2415
jagdishjnext <jagdish.jnext@gmail.com>
2025-02-27 07:42:43 +0530

    fix:use environment variable for Google OAuth redirect URL

--- controllers/oauth.go
+++ controllers/oauth.go
@@ -21,7 +21,7 @@ import (
 var googleOauthConfig = &oauth2.Config{
 	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 	ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-	RedirectURL:  "http://localhost:8080/auth/google/callback",
+	RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 	Endpoint:     google.Endpoint,
 }
